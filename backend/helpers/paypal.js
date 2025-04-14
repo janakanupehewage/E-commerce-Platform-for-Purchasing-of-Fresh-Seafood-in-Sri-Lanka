2f8cb44a7f42a1de3bd18ab1fd4a4090d0b2bfeb
janakanupehewage <janakanupehewage02@gmail.com>
2025-04-14 08:20:26 +0530

    env added

--- backend/helpers/paypal.js
+++ backend/helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
     mode : "sandbox",
-    client_id : "AS8aCViNHy9eKazgp46LaLFn_2n3VoRnIRJNJaY99HIJUykKOovZrwopDbPlPsMXIDVvGg0Bkt-2zCvk",
-    client_secret : "EEF0QEvHONCKOH-t1rYJ4FSk9p2uFWKurrVbzUD-cwcCcs87mIgDcLA4kA4xff_lGSw-3wvVeQnUvv_o"
+    client_id : process.env.PAYPAL_CLIENT_ID,
+    client_secret : process.env.PAYPAL_CLIENT_SECRET
 });
 
 module.exports = paypal;
