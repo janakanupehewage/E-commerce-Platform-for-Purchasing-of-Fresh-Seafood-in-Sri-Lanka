e2b414239309b72814e395b6152fe2548fe23b3e
janakanupehewage <janakanupehewage02@gmail.com>
2025-04-23 16:32:30 +0530

    auth controller logic changed

--- backend/controllers/auth/auth-controller.js
+++ backend/controllers/auth/auth-controller.js
@@ -101,10 +101,15 @@ const loginUser = async (req, res) => {
   //logout
   
   const logoutUser = (req, res) => {
-    res.clearCookie("token").json({
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    }).json({
       success: true,
       message: "Logged out successfully!",
     });
+    
   };
   
   //auth middleware
